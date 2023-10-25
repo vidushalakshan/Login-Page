ece5df1fa1d9296b27b8f991437bc1d7e3a4ce04
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-10-25 16:24:00 +0530

    create login page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import LoginSignUp from './Components/LoginSignup/LoginSignUp'
+
+function App() {
+
+  return (
+    <>
+      <LoginSignUp/>
+    </>
+  )
+}
+
+export default App
