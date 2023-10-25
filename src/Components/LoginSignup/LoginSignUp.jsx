94f64f3fd0753466070d370d96703b937b5143a8
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-10-25 16:46:35 +0530

    updated text fields

--- src/Components/LoginSignup/LoginSignUp.jsx
+++ src/Components/LoginSignup/LoginSignUp.jsx
@@ -29,7 +29,7 @@ const LoginSignUp = () => {
               d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
             ></path>
           </svg>
-          <input type="text" className="" />
+          <input type="text" placeholder="Name" />
         </div>
         <div className="input">
           <svg
@@ -47,7 +47,7 @@ const LoginSignUp = () => {
               d="M21.75 9v.906a2.25 2.25 0 01-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 001.183 1.981l6.478 3.488m8.839 2.51l-4.66-2.51m0 0l-1.023-.55a2.25 2.25 0 00-2.134 0l-1.022.55m0 0l-4.661 2.51m16.5 1.615a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V8.844a2.25 2.25 0 011.183-1.98l7.5-4.04a2.25 2.25 0 012.134 0l7.5 4.04a2.25 2.25 0 011.183 1.98V19.5z"
             ></path>
           </svg>
-          <input type="email" className="" />
+          <input type="email" placeholder="Email" />
         </div>
         <div className="input">
           <svg
@@ -65,7 +65,7 @@ const LoginSignUp = () => {
               d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
             ></path>
           </svg>
-          <input type="password" className="" />
+          <input type="password" placeholder="Password" />
         </div>
       </div>
 
